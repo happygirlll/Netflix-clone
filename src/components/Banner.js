3d5b150451f6dd10e5be2eb20edf2ccb45df10ff
happygirlll <lbm000314@gmail.com>
2024-12-06 23:46:08 +0900

    [넷플릭스] styled component 이용해서 비디오 배너 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../styles/Banner.css'
 import requests from '../api/requests';
 import axios from '../api/axios';
-
+import styled from 'styled-components';
 
 const Banner = () => {
     //useState 사용 : movie 상태 생성, 빈 배열 설정 | setMovie로 movie 상태 업데이트
@@ -41,12 +41,12 @@ const Banner = () => {
 
             if (randomMovieId) {
                 const { data: movieDetail } = await axios.get(
-                            `movie/${randomMovieId}`,
-                            {
-                                params: { append_to_response: 'videos' },
-                            });
-                        setMovie(movieDetail);
-                    }
+                    `movie/${randomMovieId}`,
+                    {
+                        params: { append_to_response: 'videos' },
+                    });
+                setMovie(movieDetail);
+            }
         } catch (error) {
             console.error("데이터를 불러오지 못합니다.", error);
         }
@@ -55,38 +55,80 @@ const Banner = () => {
         }
     };
 
-    if(!isClicked) {
+    if (!isClicked) {
+        return (
+            <section className="main-banner"
+                style={{
+                    backgroundImage: movie?.backdrop_path
+                        ? `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`
+                        : `url("/default-banner-image.jpg")`, backgroundSize: 'cover',
+                    backgroundPosition: 'Top center',
+                }}
+            >
+                {loading ? (
+                    <div className="loader">로딩 중...</div>
+                ) : (
+                    <div className="banner-content">
+                        <h1 className='banner-titile'>{movie?.title || movie?.name || movie?.original_name}</h1>
+                        <div className="buttons">
+                            <button className="banner-play-btn"
+                                onClick={() => setIsClicked(true)}>재생</button>
+                            <button className="banner-detail-btn">상세 정보</button>
+                        </div>
+                    </div>
+                )}
+            </section>
+        );
+    } else {
+        // Iframe?
+        /* 아이프레임은 HTML inline frame 요소이며, inline frame의 약자이다.
+        다른 html 페이지를 현재 페이지에 포함시키는 중첩된 브라우저로
+        아이프레임 요소를 이용하면 어떠한 제한 없이 다른 페이지를 불어와서 삽입 가능 */
         return (
             <Container>
                 <HomeContainer>
-                    
+                    <Iframe
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0]?.key}
+                        ?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0]?.key}`}
+                        width="640"
+                        height="360"
+                        framborder="0"
+                        allow="autoplay; fullscreen">
+                    </Iframe>
                 </HomeContainer>
             </Container>
         )
-
-    return (
-        <section className="main-banner"
-            style={{
-                backgroundImage: movie?.backdrop_path
-                    ? `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`
-                    : `url("/default-banner-image.jpg")`, backgroundSize: 'cover',
-                backgroundPosition: 'Top center',
-            }}
-        >
-            {loading ? (
-                <div className="loader">로딩 중...</div>
-            ) : (
-                <div className="banner-content">
-                    <h1 className='banner-titile'>{movie?.title || movie?.name || movie?.original_name}</h1>
-                    <div className="buttons">
-                        <button className="banner-play-btn"
-                                onClick={() => setIsClicked(true)}>재생</button>
-                        <button className="banner-detail-btn">상세 정보</button>
-                    </div>
-                </div>
-            )}
-        </section>
-    );
+    }
 };
 
 export default Banner;
+
+// styled-components로 Banner 컴포넌트를 스타일링
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    flex-direction: column;`;
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;`;
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    border: none;
+    opacity: 0.65;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;    
+    }
+`;
