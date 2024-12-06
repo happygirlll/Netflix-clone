18c75cd7fe142656182f2fabb4fc6c2613790710
happygirlll <lbm000314@gmail.com>
2024-12-06 10:44:21 +0900

    [넷플릭스] nav, banner, row, footer 초기 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Banner/>
+      <Row/>
+      <Footer/>
     </div>
   );
 }
