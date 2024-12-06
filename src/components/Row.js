b5353c2a2e90e851f4d96655ab049d1671c3ff08
happygirlll <lbm000314@gmail.com>
2024-12-07 01:28:23 +0900

    [넷플릭스] row - jsx 작성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,20 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
 
-export default function Row({title, fetchUrl}) {
+export default function Row({ title, fetchUrl, isLargeRow, id }) {
 
     const [movies, setMovies] = useState([]);
     useEffect(() => {
-        fetchMoviesData();
+        const fetchMovies = async () => {
+            try {
+                const { data } = await axios.get(fetchUrl);
+                setMovies(data.results || []);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchMovies();
     }, [fetchUrl]);
 
-    const fetchMoviesData = async () => {
-        const response = await axios.get(fetchUrl);
-        setMovies(response.data.results);
-        return response;
+
+    const getImageUrl = (movie) => {
+        const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+        return `${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`;
     };
 
     return (
-        <div>Row</div>
+        <section className="row">
+            <h2 className="row-title">{title}</h2>
+
+            <div className="slider-container">
+                <button className="slider-arrow left-arrow">{"<"}</button>
+                
+                <div id={id} className="posters-container">
+                    {movies.map((movie) => (
+                        <img
+                            key={movie.id}
+                            className={`poster ${isLargeRow ? "large-poster" : ""}`}
+                            src={getImageUrl(movie)}
+                            alt={movie.name || movie.title || "Movie"}
+                            loading="lazy" 
+                        />
+                    ))}
+                </div>
+
+                <button className="slider-arrow right-arrow">{">"}</button>
+            </div>
+        </section>
     )
 }
