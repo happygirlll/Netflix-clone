62ebae1e8c786a34e83d039b83dae2226531315f
happygirlll <lbm000314@gmail.com>
2024-12-06 15:07:39 +0900

    [넷플릭스] nav바 로고, 프로필 위치 조정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import '../styles/Nav.css'
 
 export default function Nav() {
+
+
     return (
-        <div>Nav</div>
+        <nav className={'nav'}>
+            <img 
+                className='nav-logo'
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/400px-Netflix_2015_logo.svg.png"
+                alt="netflix-logo"
+            />
+
+            <img
+                className='nav-avatar'
+                src={process.env.PUBLIC_URL + '/Netflix-avatar.png'}
+                alt="netflix-avatar"
+            />
+        </nav>
 )
 }
