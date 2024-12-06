55afbcf58604c2cf889a53c36e71c49be5bc58f3
happygirlll <lbm000314@gmail.com>
2024-12-06 10:06:21 +0900

    [넷플릭스] Axios 인스턴스 생성

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        apiKey: "55dfbda229955ae4c4efd2d50dedd1ab",
+        language: "ko-KR",
+    },
+});
+
+export default instance;
