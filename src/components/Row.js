452c561758de4bb907265798600bbb9172cd496e
happygirlll <lbm000314@gmail.com>
2024-12-07 00:48:57 +0900

    [넷플릭스] fetchMovieData 영화 정보 가져오기

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from '../api/axios';
 
-export default function Row() {
+export default function Row({title, fetchUrl}) {
+
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        fetchMoviesData();
+    }, [fetchUrl]);
+
+    const fetchMoviesData = async () => {
+        const response = await axios.get(fetchUrl);
+        setMovies(response.data.results);
+        return response;
+    };
+
     return (
         <div>Row</div>
     )
