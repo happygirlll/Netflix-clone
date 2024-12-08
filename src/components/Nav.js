6925fbc94b2c87b02ce55ecb1f18a4d59ee8ac0d
happygirlll <lbm000314@gmail.com>
2024-12-08 17:13:51 +0900

    [넷플릭스] nav바 nav-menu 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../styles/Nav.css'
 
 export default function Nav() {
+
     return (
         <nav className={'nav'}>
             <img 
@@ -9,6 +10,15 @@ export default function Nav() {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/400px-Netflix_2015_logo.svg.png"
                 alt="netflix-logo"
             />
+
+            <div className="nav-menu">
+                <a href='http://localhost:3000/'>홈</a>
+                <a href='http://localhost:3000/'>시리즈</a>
+                <a href='http://localhost:3000/'>영화</a>
+                <a href='http://localhost:3000/'>NEW! 요즘 대세 콘텐츠</a>
+                <a href='http://localhost:3000/'>내가 찜한 리스트</a>
+                <a href='http://localhost:3000/'>언어별로 찾아보기</a>
+            </div>
 
             <img
                 className='nav-avatar'
